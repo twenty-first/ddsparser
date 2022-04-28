721c7608bd5c568de08b8a24133d81bd108e5c12
Nicola Musatti <nicola.musatti@gmail.com>
2022-04-28 08:32:12 +0200

    Conform to file naming convention

--- src/test/java/it/twenfir/ddsparser/AstUnitTests.java
+++ src/test/java/it/twenfir/ddsparser/AstUnitTests.java
@@ -13,11 +13,11 @@ import org.slf4j.LoggerFactory;
 import it.twenfir.ddsparser.ast.Dds;
 import it.twenfir.ddsparser.ast.Field;
 
-public class AstTest extends TestBase {
+public class AstUnitTests extends TestBase {
 
-	static final Logger log = LoggerFactory.getLogger(AstTest.class);
+	static final Logger log = LoggerFactory.getLogger(AstUnitTests.class);
 
-	public AstTest() {
+	public AstUnitTests() {
 		super(log);
 	}
 
--- src/test/java/it/twenfir/ddsparser/ParserUnitTests.java
+++ src/test/java/it/twenfir/ddsparser/ParserUnitTests.java
@@ -6,11 +6,11 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ParserTest extends TestBase {
+public class ParserUnitTests extends TestBase {
 
-	private static final Logger log = LoggerFactory.getLogger(ParserTest.class);
+	private static final Logger log = LoggerFactory.getLogger(ParserUnitTests.class);
 	
-	public ParserTest() {
+	public ParserUnitTests() {
 		super(log);
 	}
 
